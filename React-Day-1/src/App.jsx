6e10753e9234f96254c369ea5009eb27e3c0e5cf
Nicolas K <gabnicol0807@gmail.com>
2025-12-09 19:27:47 +0700

    Project React terbaru

--- React-Day-1/src/App.jsx
+++ React-Day-1/src/App.jsx
@@ -4,18 +4,11 @@ import Header from './components/Header';
 function App() {
   return (
     <div style={{ textAlign: 'center', fontFamily: 'Arial', paddingBottom: '50px' }}>
-      
-      {/* BAGIAN 1: LATIHAN TASBIH */}
       <h1>--- Project 1: Tasbih Digital ---</h1>
       <Tasbih />
-
       <br /> <hr /> <br />
-
-      {/* BAGIAN 2: LATIHAN KARTU YONKO (Props + State) */}
       <h1>--- Project 2: Voting Karakter ---</h1>
       <p>Coba like salah satu, yang lain tidak akan ikut berubah.</p>
-      
-      {/* Kita panggil komponen Header berkali-kali dengan data beda */}
       <Header nama="Kaido" judul="Yonko" status="Top Tier" />
       <Header nama="Akainu" judul="Admiral" status="Top Tier" />
       <Header nama="Buggy" judul="Yonko" status="Lucky Tier" />
