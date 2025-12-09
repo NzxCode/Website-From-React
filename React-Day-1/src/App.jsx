64c8ad914888ae303611f6e7159eb0cdd5fcd27f
Nicolas K <gabnicol0807@gmail.com>
2025-12-09 18:23:35 +0700

    Update terbaru cuy

--- React-Day-1/src/App.jsx
+++ React-Day-1/src/App.jsx
@@ -1,14 +1,34 @@
-import Header from './components/Header';
+import { useState } from 'react';
 
 function App() {
-  const nama = "Nicolas";
-return (
-  <div>
-    <h1>Hi! nama gw {nama} List-List Props Terbaik! Dalam hal top tiernya</h1>
-    <Header judul ="Yonko" status ="Top Tier" nama ="Kaido" />
-    <Header judul ="Admiral" status ="Top Tier" nama ="Akainu" />
-  </div>
+  const [App, setApp] = useState({
+    brand: "Nvidia",
+    model: "Prototype",
+    year: "2007",
+    color: "Biru"
+  });
+
+  function SapaUser() {
+    console.log("Tombol berhasil ditekan! Pesan ini dari Console.");
+    alert("Tombol ditekan!");
+  }
+
+  const updateColor = () => {
+    setApp(previousState => {
+      return {... previousState, color : "yellow" }
+    });
+  }
+
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Selamat Datang di Web {App.brand}</h1>
+      <p>Brand ini sudah ada mulai dari model {App.model} yang berwarna {App.color} dibuat pada tahun {App.year}</p>
+      <button type="button" onClick={updateColor} style={{ padding: '10px 20px', fontSize: '16px' }}>Klik Saya!</button>
+      <button onClick={sapaUser} style={{ padding: '10px 20px', fontSize: '16px' }}>
+        Coba Sapa!
+      </button>
+    </div>
   )
 }
 
-export default App;
+export default App
