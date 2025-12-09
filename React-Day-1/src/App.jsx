b9dbe1baae1f979b3e2af01e454081dd2af3decc
Nicolas K <gabnicol0807@gmail.com>
2025-12-09 19:09:02 +0700

    Update react terbaru

--- React-Day-1/src/App.jsx
+++ React-Day-1/src/App.jsx
@@ -1,43 +1,14 @@
 import { useState } from 'react';
+import Tasbih from './src/Tasbih';
 
 function App() {
-  const [data, setData] = useState({
-    brand: "Nvidia",
-    model: "RTX 4090",
-    year: "2024",
-    color: "Biru"
-  });
-
-  function SapaUser() {
-    console.log("Tombol ditekan!");
-    alert("Halo Nicolas!");
-  }
-
-  // Fungsi Ubah Warna
-  const updateColor = () => {
-    setData(previousState => {
-      return { ...previousState, color: "Yellow (Emas)" }
-    });
-  }
-
   return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>Selamat Datang di Web {data.brand}</h1>
-      <p>
-        Model: {data.model} | Tahun: {data.year} | Warna: 
-        <b style={{ color: data.color === 'Biru' ? 'blue' : 'gold' }}> {data.color}</b>
-      </p>
-      
-      <div style={{ marginTop: '20px' }}>
-        <button onClick={updateColor} style={{ marginRight: '10px', padding: '10px' }}>
-          Ubah Warna
-        </button>
-        <button onClick={SapaUser} style={{ padding: '10px' }}>
-          Sapa Saya
-        </button>
-      </div>
+    <div style={{ textAlign: 'center', fontFamily: 'Arial' }}>
+      <h1>Latihan React Day 2</h1>
+      <p>Di bawah ini adalah komponen Counter dari file terpisah:</p>
+      <Tasbih />
     </div>
   )
 }
 
-export default App
+export default App;
