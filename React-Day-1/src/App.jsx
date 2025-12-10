d2e90302fb76d5c9787f83b57123a86a31948171
Nicolas K <gabnicol0807@gmail.com>
2025-12-10 17:05:57 +0700

    Update terbaru buat react

--- React-Day-1/src/App.jsx
+++ React-Day-1/src/App.jsx
@@ -1,18 +1,9 @@
-import Tasbih from './Tasbih';   
-import Header from './components/Header';  
+import Menu from './Main_Menu'; 
 
 function App() {
   return (
-    <div style={{ textAlign: 'center', fontFamily: 'Arial', paddingBottom: '50px' }}>
-      <h1>--- Project 1: Tasbih Digital ---</h1>
-      <Tasbih />
-      <br /> <hr /> <br />
-      <h1>--- Project 2: Voting Karakter ---</h1>
-      <p>Coba like salah satu, yang lain tidak akan ikut berubah.</p>
-      <Header nama="Kaido" judul="Yonko" status="Top Tier" />
-      <Header nama="Akainu" judul="Admiral" status="Top Tier" />
-      <Header nama="Buggy" judul="Yonko" status="Lucky Tier" />
-
+    <div>
+       <Menu />
     </div>
   )
 }
