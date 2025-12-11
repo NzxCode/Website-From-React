67789ef9c24a5a441fc4448ec2e809734d37f70b
Nicolas K <gabnicol0807@gmail.com>
2025-12-11 13:08:33 +0700

    Update react projectnya

--- React-Day-1/src/App.jsx
+++ React-Day-1/src/App.jsx
@@ -1,10 +1,13 @@
+import {Routes, Route } from 'react-router-dom';
 import Menu from './Main_Menu'; 
+import CoinDetail from './components/CoinDetail';
 
 function App() {
   return (
-    <div>
-       <Menu />
-    </div>
+    <Routes>
+      <Route path="/" element={<Menu />} />
+      <Route path="/coin/:id" element={<CoinDetail />} />
+    </Routes>
   )
 }
 
---
+++ React-Day-1/src/components/CoinDetail.jsx
@@ -0,0 +1,10 @@
+function CoinDetail () {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>Halaman Detail!</h1>
+            <p>Nanti data detail coin-coinnya dan grafiknya akan muncul disini!</p>
+        </div>
+    )
+}
+
+export default CoinDetail;
